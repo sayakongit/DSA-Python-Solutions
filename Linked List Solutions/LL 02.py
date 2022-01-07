fcb12c8d5cc6d68bbb0d43bb3de991b38650850e
Sayak Saha <sayak.saha.fiem.ece19@teamfuture.in>
2022-01-08 03:28:51 +0530

    Linkedin Solution 1 added

---
+++ Linked List Solutions/LL 02.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, data=None, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+class DoublyLL():
+    def __init__(self):
+        self.head = None
+
+    def print_forward(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        itr = self.head
+        llstr = ''
+        while itr:
+            llstr += str(itr.data) + ' --> ' if itr.next else str(itr.data)
+            itr = itr.next
+        print(llstr)
+
+
+
+if __name__ == '__main__':
+    pass
